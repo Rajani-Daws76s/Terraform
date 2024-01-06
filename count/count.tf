027282b96b24322ba487a610293068614fc802aa
rajani <anilkumar.guntla@gmail.com>
2024-01-06 11:37:06 +0530

    terraform practice

--- count/count.tf
+++ count/count.tf
@@ -1,5 +1,6 @@
 resource "aws_instance" "rajani" {
-  count  = 11 #count/index is special variable given by teraaform
+  #count  = 11 #count/index is special variable given by teraaform
+  count = length(var.instance_names)
   ami           = var.ami-id
   instance_type = var.instance_names[count.index] == "mongodb" || var.instance_names[count.index] == "shipping" || var.instance_names[count.index] == "mysql" ? "t3.small" : "t2.micro"
   tags  = {
@@ -9,7 +10,8 @@ resource "aws_instance" "rajani" {
 
 # this is for creating route 53 records
 resource "aws_route53_record" "www" {
-  count  = 11
+  #count  = 11
+  count = length(var.instance_names)
   zone_id = var.zone_id
   name    = "${var.instance_names[count.index]}.${var.domain_name}" # interpolation is combination variable and text 
   type    = "A"
